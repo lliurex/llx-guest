1893004f1548736c4f91ef6b71b799f43584bdd4
cless <hectorgh@gmail.com>
2019-10-21 13:36:06 +0200

    multiple changes

--- llx-guest-gui.install/usr/share/llx-guest/LlxGuest.py
+++ llx-guest-gui.install/usr/share/llx-guest/LlxGuest.py
@@ -3,54 +3,28 @@
 
 import gi
 gi.require_version('Gtk', '3.0')
-from gi.repository import Gtk,GObject, GLib, Gdk
+from gi.repository import Gtk,GObject, GLib, Gdk,Gio
 
 import signal
 import gettext
 import sys
 import threading
 import time
-import GuestUser as GuestUser
+import os
+
 
 signal.signal(signal.SIGINT, signal.SIG_DFL)
 #gettext.textdomain('lliurex-perfilreset')
 _ = gettext.gettext
 
-class Spinner:
-    busy = False
-    delay = 0.1
 
-    @staticmethod
-    def spinning_cursor():
-        while 1: 
-            for cursor in '|/-\\': yield cursor
-
-    def __init__(self, delay=None):
-        self.spinner_generator = self.spinning_cursor()
-        if delay and float(delay): self.delay = delay
-
-    def spinner_task(self):
-        while self.busy:
-            sys.stdout.write(next(self.spinner_generator))
-            sys.stdout.flush()
-            time.sleep(self.delay)
-            sys.stdout.write('\b')
-            sys.stdout.flush()
-
-    def start(self):
-        self.busy = True
-        threading.Thread(target=self.spinner_task).start()
-
-    def stop(self):
-        self.busy = False
-        time.sleep(self.delay)
-
-#class spinner
 
 
 class LlxGuest:
 
-	DEBUG= True
+	DEBUG= False
+	CSS_FILE="/usr/share/llx-guest/rsrc/style.css"
+	CSS_FILE="/home/lliurex/llx-guest/llx-guest-gui.install/usr/share/llx-guest/style.css"
 
 	def printd (self,text):
 
@@ -63,7 +37,7 @@ class LlxGuest:
 	def __init__(self):
 		
 		self.llx_guest_bin="/usr/sbin/llx-guest-gui"
-		
+		self.switch_guest_error_state=False
 		
 		self.start_gui()
 		GObject.threads_init()
@@ -79,37 +53,27 @@ class LlxGuest:
 		self.main_window=builder.get_object("main_window")
 		self.main_window.set_resizable(False)
 		self.main_window.set_icon_from_file('/usr/share/llx-guest/rsrc/llx-guest-icon.svg')
-		self.main_window.set_name("WINDOW")
+		
 		self.main_box=builder.get_object("main_box")
-
 		self.close_button=builder.get_object("close_button")
-		self.close_button.set_name("CLOSE_BUTTON")
 		self.msg_label=builder.get_object("msg_label")
-		self.msg_label.set_name("MSG_LABEL")
+		self.info_label=builder.get_object("info_label")
 
 		self.switch_guest=builder.get_object("switch_guest")
 		self.switch_guest_label=builder.get_object("switch_guest_label")
-
-		self.reveal=Gtk.Revealer()
-		self.reveal.set_name("WHITE_BACKGROUND")
-		self.reveal.set_transition_duration(1)
-
-		lbl_rvl=Gtk.Label(_("Generating guest user, please wait..."))
-		lbl_rvl.set_name("NOTIF_LABEL")
-		lbl_rvl.set_hexpand(False)
-		lbl_rvl.set_vexpand(False)
-		lbl_rvl.set_halign(Gtk.Align.CENTER)
-		lbl_rvl.set_valign(Gtk.Align.CENTER)
-		self.reveal.add(lbl_rvl)
-		self.main_box.attach(self.reveal,0,0,1,4)
+		
+		self.spinner=builder.get_object("spinner")
 
 		self.lock_quit=False
+		
+		self._set_css_info()
+		
 		self.switch_initial_state()
 		self.connect_signals()
-		self._set_css_info()
-
-		self.msg_label.hide()
+		
 		self.main_window.show_all()
+		self.spinner.hide()
+		#self.msg_label.hide()
 		
 
 	#def start_gui
@@ -120,15 +84,18 @@ class LlxGuest:
 		
 		self.main_window.connect("destroy",self.close_button_clicked)
 		#If process is alive destroy window is unable
-		self.main_window.connect("delete_event",self.unable_quit)
+		self.main_window.connect("delete_event",self.window_close_clicked)
 		self.close_button.connect("clicked",self.close_button_clicked)
 		self.switch_guest.connect("state_set",self.switch_guest_modify)
+		
 
 		
 	#def connect_signals
 
 
 	def switch_initial_state(self):
+		
+		'''
 		try:
 			if GuestUser.check_permission()[0]:
 				self.switch_guest_error_state=False
@@ -154,16 +121,42 @@ class LlxGuest:
 			self.printd("You don't have admin permissions to change files. Please contact with the administrator")
 			self.printd ("%s"%e)
 			return [False,str(e)]
+		'''
+		
+		if self.get_guest_status():
+			self.switch_guest.set_state(True)
+		
+		if not self.check_permissions():
+			self.switch_guest.set_sensitive(False)
+			self.msg_label.set_text(_("You don't have privileges to enable or disable guest user."))
+			
+		
+		return True
+		
 
 	# def_switch_initial_state
 
+
+	def _set_css_info(self):
+	
+		self.style_provider=Gtk.CssProvider()
+		f=Gio.File.new_for_path(LlxGuest.CSS_FILE)
+		self.style_provider.load_from_file(f)
+		Gtk.StyleContext.add_provider_for_screen(Gdk.Screen.get_default(),self.style_provider,Gtk.STYLE_PROVIDER_PRIORITY_APPLICATION)
+		
+		
+		self.main_window.set_name("WINDOW")
+		self.close_button.set_name("CLOSE_BUTTON")
+		self.msg_label.set_name("MSG_LABEL")
+		self.info_label.set_name("INFO_LABEL")
+		
+	#def set_css_info	
 
 
 	def close_button_clicked(self,widget=True):
 		
 		if self.lock_quit:
-			self.msg_label.set_text(_("You must wait to end the process......keep calm."))
-			self.printd("You must wait to end the process......keep calm.")
+			self.msg_label.set_text(_("Please wait until the process is finished"))
 			self.msg_label.show()
 		else:	
 			Gtk.main_quit()
@@ -171,18 +164,35 @@ class LlxGuest:
 		
 	#def check_changes
 
+
+	def window_close_clicked(self,widget,event):
+
+		if self.lock_quit:
+			self.msg_label.set_text(_("Please wait until the process is finished"))
+			self.msg_label.show()
+			return True
+		else:
+			return False
+			
+	#def unable_quit
+	
+
 	def switch_guest_modify(self,widget,gparam):
 		if self.switch_guest_error_state:
 			return True
+
 			
 		self.switch_guest.set_sensitive(False)
-		self.main_window.connect
+		self.close_button.set_sensitive(False)
 		self.lock_quit=True
-		self.msg_label.set_name("MSG_LABEL_DELETE")
-		self.msg_label.set_text(_("Please wait until the process is finished....."))
+		#self.msg_label.set_name("MSG_LABEL_DELETE")
+		self.msg_label.set_text(_("Please wait until the process is finished"))
+		self.spinner.show()
 		th=threading.Thread(target=self.th_add_guest_user)
 		GLib.timeout_add(1000,self.show_reveal,th)
 		th.start()
+		
+		return False
 
 	#def reset_clicked
 	
@@ -192,14 +202,16 @@ class LlxGuest:
 		self.switch_guest_error_state=False
 		if self.switch_guest.get_active():
 			self.printd("Adding guest user...")
-			if GuestUser.add_guest_user()[0]:
+			#if GuestUser.add_guest_user()[0]:
+			if self.enable_guest():
 				self.state=True
 			else:
 				self.state=False
 				self.switch_guest_error_state=True
 		else:
 			self.printd("Deleting guest user...")
-			if GuestUser.delete_guest_user()[0]:
+			#if GuestUser.delete_guest_user()[0]:
+			if self.disable_guest():
 				self.state=False
 			else:
 				self.state=True
@@ -209,7 +221,7 @@ class LlxGuest:
 
 		#self.printd("Switch was turned %s"%self.state)
 
-		time.sleep(1)
+		#time.sleep(1)
 
 	def show_reveal(self,*args):
 		th=args[-1]
@@ -218,26 +230,24 @@ class LlxGuest:
 			return True
 
 		self.printd("Thread FINISHED.")
-
-		#spinner.stop()
-		#self.reveal.set_reveal_child(False)
+		
+		self.switch_guest.set_sensitive(True)
+		self.close_button.set_sensitive(True)
+		self.spinner.hide()
 		
 		self.lock_quit=False
 		if self.switch_guest_error_state:
 			self.msg_label.set_name("MSG_LABEL")
-			self.msg_label.set_text(_("LlX-Guest user has been a problem to modify user files\nPlease contact with the administrator system."))
-			self.printd("LlX-Guest user has been a problem to modify user files. Please contact with the administrator system.")
+			self.msg_label.set_text(_("There has been a problem creating guest user."))
 			self.switch_guest.set_state(self.state)
 		else:
-			self.switch_guest.set_sensitive(True)
+			
 			if self.state:
 				self.msg_label.set_name("MSG_LABEL")
-				self.msg_label.set_text(_("Guest user added\nYou must restart the session to try it."))
-				self.printd("Guest user added. You must restart the session to try it.")
+				self.msg_label.set_text(_("Guest user added."))
 			else:
 				self.msg_label.set_name("MSG_LABEL")
-				self.msg_label.set_text(_("Guest user deleted"))
-				self.printd("Guest user deleted.")
+				self.msg_label.set_text(_("Guest user deleted."))
 			
 			self.switch_guest_error_state=False
 		self.msg_label.show()
@@ -246,82 +256,59 @@ class LlxGuest:
 
 	# ##################### ##########################################
 
-	def unable_quit(self,widget,event):
 
-		if self.lock_quit:
-			self.msg_label.set_text(_("You must wait to end the process......keep calm."))
-			self.printd("You must wait to end the process......keep calm.")
-			self.msg_label.show()
+
+	def get_guest_status(self):
+		
+		ret=os.system("llx-guest-manager status")
+		
+		if ret==0:
+			return True
+		
+		
+		return False
+		
+	#def get_guest_state
+	
+	
+	def enable_guest(self):
+		
+		ret=os.system("llx-guest-manager enable")
+		
+		if ret==0:
+			self.state=True
 			return True
 		else:
+			self.switch_guest_error_state=True
 			return False
-			
-	#def unable_quit
-
-
-	def _set_css_info(self):
+		
+	#def enable_guest
 	
-		css = b"""
-
-		GtkLabel {
-			font-family: Roboto;
-		}
-
-		#WINDOW{
+	
+	def disable_guest(self):
 		
-		background-color: #ffffff;
+		ret=os.system("llx-guest-manager disable")
 		
-		}
-
-		#NOTIF_LABEL{
-			background: #3366cc;
-			font: 11px Roboto;
-			color:white;
-			border: dashed 1px silver;
-			padding:6px;
-		}
-
-		#MSG_LABEL {
-			color: #24478f;
-			font: 11pt Roboto Bold;
-
-		}
-
-		#MSG_LABEL_DELETE {
-			color: red;
-			font: 11pt Roboto Bold;
-
-		}
-
-		#WHITE_BACKGROUND {
-			background: rgba(1,1,1,0);
-			box-shadow: 1px 1px 1px 10px white;
+		if ret==0:
+			self.state=False
+			return True
+		else:
+			self.switch_guest_error_state=True
+			return False
 		
-		}
-
-		#CLOSE_BUTTON{
-			border-width: 0px;
-			box-shadow: none;
-			border-color: #76819a;
-			background-image:-gtk-gradient (linear,	left top, left bottom, from (#76819a),  to (#76819a));
-			font: 11pt Roboto Light;
-			text-shadow: none;
-			color: rgba(0,0,0,0.7);
-		}
-		#CLOSE_BUTTON:hover {
-			border-color: #209ddd;
-			background-image:-gtk-gradient (linear,	left top, left bottom, from (#209ddd),  to (#209ddd));
-			box-shadow: -0.5px 3px 3px #aaaaaa;
-			font: 11pt Roboto Light;
-			text-shadow: none;
-			color: rgba(255,255,255,1);
-		}
-
-		"""
-		self.style_provider=Gtk.CssProvider()
-		self.style_provider.load_from_data(css)
-		Gtk.StyleContext.add_provider_for_screen(Gdk.Screen.get_default(),self.style_provider,Gtk.STYLE_PROVIDER_PRIORITY_APPLICATION)
-	#def set_css_info		
+	#def disable_user
+	
+	
+	def check_permissions(self):
+		
+		try:
+			f=open("/run/llx-guest.run","w")
+			f.close()
+			return True
+		except:
+			return False
+		
+	#def check_permissions
 
 
 #class LlxGuest
--- translations/generate_po.sh
+++ translations/generate_po.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-UI_FILES="../llx-guest.install/usr/share/llx-guest/rsrc/llx-guest.ui "
-PYTHON_FILES="../llx-guest.install/usr/share/llx-guest/LlxGuest.py"
+UI_FILES="../llx-guest-gui.install/usr/share/llx-guest/rsrc/llx-guest.ui"
+PYTHON_FILES="../llx-guest-gui.install/usr/share/llx-guest/LlxGuest.py"
 
 xgettext $UI_FILES $PYTHON_FILES -o llx-guest/llx-guest.pot
 
